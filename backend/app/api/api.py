63121c5a76f8cabc551ab603fcf4f735e8b4ee02
Yanli Wang <wang970753651@gmail.com>
2023-10-24 20:48:49 -0400

    add event button
    UI is ugly, will work on it later

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,14 +1,32 @@
-import os
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime, timedelta
 
 app = FastAPI()
 
-origins = ['http://localhost', 'localhost']
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development, you might want to restrict this later!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-@app.post("/myevent")
-async def root():
-    return {"start": int(datetime.now().timestamp()), "end": int((datetime.now() + timedelta(hours=1)).timestamp()), "title": "Going to Your Mom's House"}
+class Event(BaseModel):
+    start: int
+    end: int
+    title: str
+    description: str
+
+events_data = []
+
+@app.get("/api")
+def get_events():
+    return events_data
+
+@app.post("/api")
+def add_event(event: Event):
+    events_data.append(event.dict())
+    return event
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,91 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { useEffect, useState } from 'react';
+
+const BACKEND_URL = 'http://localhost:8080';
 
 function App() {
   const [events, setEvents] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
-  useEffect(()=>{
-    fetch('/api/myevent', {method: "POST"}).then(response => response.json()).then(response => setEvents([{'start': moment.unix(response.start).toDate(), 'end': moment.unix(response.end).toDate(), 'title': response.title}]));
+  const toggleModal = () => {
+    setShowModal(prev => !prev);
+  }
+
+  useEffect(() => {
+    fetch(`${BACKEND_URL}/api`, { method: "GET" })
+      .then(response => response.json())
+      .then(data => {
+        const formattedEvents = data.map(event => ({
+          start: moment.unix(event.start).toDate(),
+          end: moment.unix(event.end).toDate(),
+          title: event.title
+        }));
+        setEvents(formattedEvents);
+      });
   }, []);
 
+  const handleAddEvent = () => {
+    const newEvent = {
+      start: moment(startTime).unix(),
+      end: moment(endTime).unix(),
+      title: title,
+      description: description
+    };
+
+    fetch(`${BACKEND_URL}/api`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newEvent)
+    })
+    .then(response => response.json())
+    .then(data => {
+      setEvents(prevEvents => [...prevEvents, {
+        start: moment.unix(data.start).toDate(),
+        end: moment.unix(data.end).toDate(),
+        title: data.title
+      }]);
+    });
+
+    setTitle('');
+    setDescription('');
+    setStartTime('');
+    setEndTime('');
+    toggleModal();
+  }
 
   return (
-    <Calendar localizer={momentLocalizer(moment)} events={events} defaultDate={new Date()} defaultView="month" style={{ height: "100vh" }} />
+    <div>
+      <Calendar 
+        localizer={momentLocalizer(moment)} 
+        events={events} 
+        defaultDate={new Date()} 
+        defaultView="month" 
+        style={{ height: "100vh" }} 
+      />
+      <button onClick={toggleModal}>Add Event</button>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={toggleModal}>&times;</span>
+            <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Title" />
+            <input value={description} onChange={e => setDescription(e.target.value)} placeholder="Description" />
+            <input type="datetime-local" value={startTime} onChange={e => setStartTime(e.target.value)} />
+            <input type="datetime-local" value={endTime} onChange={e => setEndTime(e.target.value)} />
+            <button onClick={handleAddEvent}>Save Event</button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
