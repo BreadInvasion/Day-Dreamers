008babf918f8a93644abf9cc52ebc61230fb7885
Yanli Wang <114595115+yanliw123@users.noreply.github.com>
2023-10-30 17:45:07 -0400

    [Feature] Add event button (#1)
    * add event button
    
    UI is ugly, will work on it later
    
    * update
    
    * Update App.css
    
    * move 'add event' button to right
    
    * add title in popup window.
    
    * Delete event function
    
    Add event select and delete function
    
    * Add dependency "node-sass"
    
    * Drag and Drop Function
    
    Move event to another time slot.
    Drag side to extend the event.
    Add necessary dependencies.
    
    * Drag create
    
    Drag on slot to create new event
    
    * Update App.js
    
    * Add minimum distance for  drag create
    
    * Update App.js
    
    * Updated frontend dependencies
    
    ---------
    
    Co-authored-by: BairongZhang1 <zbr784212056@gmail.com>
    Co-authored-by: zjh1379 <834943383@qq.com>
    Co-authored-by: Mark <haddlm@rpi.edu>

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,14 +1,51 @@
 import os
 from fastapi import FastAPI
+from fastapi import HTTPException
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime, timedelta
+from uuid import uuid4
 
 app = FastAPI()
 
-origins = ['http://localhost', 'localhost']
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development, you might want to restrict this later!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-@app.post("/myevent")
-async def root():
-    return {"start": int(datetime.now().timestamp()), "end": int((datetime.now() + timedelta(hours=1)).timestamp()), "title": "Going to Your Mom's House"}
+class Event(BaseModel):
+    start: int
+    end: int
+    title: str
+    description: str
+
+events_data = []
+
+@app.get("/api")
+def get_events():
+    return events_data
+
+@app.post("/api")
+def add_event(event: Event):
+    events_data.append(event.dict())
+    return event
+
+@app.put("/api/{event_id}")
+def update_event(event_id: int, event: Event):
+    try:
+        events_data[event_id] = event.dict()
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return {"status": "success"}
+
+@app.delete("/api/{event_id}")
+def delete_event(event_id: int):
+    try:
+        events_data.pop(event_id)
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return {"status": "success"}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,231 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { useEffect, useState } from 'react';
+
+const BACKEND_URL = 'http://localhost:8080';
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
 
 function App() {
   const [events, setEvents] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
-  useEffect(()=>{
-    fetch('/api/myevent', {method: "POST"}).then(response => response.json()).then(response => setEvents([{'start': moment.unix(response.start).toDate(), 'end': moment.unix(response.end).toDate(), 'title': response.title}]));
+
+
+  // Helper function to update the data
+  const fetchEvents = () => {
+    fetch(`${BACKEND_URL}/api`, { method: "GET" })
+        .then(response => response.json())
+        .then(data => {
+          const formattedEvents = data.map(event => ({
+            start: moment.unix(event.start).toDate(),
+            end: moment.unix(event.end).toDate(),
+            title: event.title
+          }));
+          setEvents(formattedEvents);
+        });
+  };
+
+  const onEventDrop = ({ event, start, end }) => {
+    const idx = events.indexOf(event);
+    const updatedEvent = {...event, start, end};
+    const nextEvents = [...events];
+    nextEvents.splice(idx, 1, updatedEvent);
+    setEvents(nextEvents);
+    handleEventDrop(event, start, end);
+  }
+  const onEventResize = ({ event, start, end }) => {
+    const idx = events.indexOf(event);
+    const updatedEvent = { ...event, start, end };
+    const nextEvents = [...events];
+    nextEvents.splice(idx, 1, updatedEvent);
+    setEvents(nextEvents);
+    handleEventResize(event, start, end);
+  };
+
+
+  const toggleModal = () => {
+    setShowModal(prev => !prev);
+  }
+
+
+
+  useEffect(() => {
+    fetchEvents();
   }, []);
 
 
+  const handleAddEvent = () => {
+    const newEvent = {
+      start: moment(startTime).unix(),
+      end: moment(endTime).unix(),
+      title: title,
+      description: description
+    };
+
+    fetch(`${BACKEND_URL}/api`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newEvent)
+    })
+    .then(response => response.json())
+    .then(data => {
+
+      setEvents(prevEvents => [...prevEvents, {
+        start: moment.unix(data.start).toDate(),
+        end: moment.unix(data.end).toDate(),
+        title: data.title
+      }]);
+      fetchEvents();
+    });
+
+    setTitle('');
+    setDescription('');
+    setStartTime('');
+    setEndTime('');
+    toggleModal();
+  }
+
+
+  const handleSelectEvent = (event) => {
+    setSelectedEvent(event);
+  }
+
+  const handleDeleteEvent = () => {
+    if (selectedEvent) {
+      const eventIndex = events.findIndex(e => e.start === selectedEvent.start && e.end === selectedEvent.end && e.title === selectedEvent.title);
+      if (eventIndex > -1) {
+        fetch(`${BACKEND_URL}/api/${eventIndex}`, {
+          method: "DELETE"
+        })
+        .then(response => {
+          if (response.ok) {
+            setEvents(prevEvents => prevEvents.filter((_, index) => index !== eventIndex));
+            setSelectedEvent(null);
+          } else {
+            console.error('Failed to delete the event');
+          }
+        })
+        .catch(error => console.error('Network error:', error));
+      }
+    }
+  }
+  const handleEventDrop = (event, start, end) => {
+    const idx = events.indexOf(event);
+    fetch(`${BACKEND_URL}/api/${idx}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        start: moment(start).unix(),
+        end: moment(end).unix(),
+        title: event.title,
+        description: event.description
+      })
+    })
+        .then(response => {
+          if (response.ok) {
+            fetchEvents();
+          } else {
+            console.error('Failed to update the event');
+          }
+        })
+        .catch(error => console.error('Network error:', error));
+  }
+
+  const handleEventResize = (event, start, end) => {
+    handleEventDrop(event, start, end);
+  }
+
+
+// Drag create
+  const handleSelectSlot = ({ start, end }) => {
+    // only create for slot > 3o minutes
+    if (moment(end).diff(moment(start), 'minutes') > 30) {
+      const newEvent = {
+        start: moment(start).unix(),
+        end: moment(end).unix(),
+        title: "",
+        description: ""
+      };
+
+      fetch(`${BACKEND_URL}/api`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newEvent)
+      })
+          .then(response => response.json())
+          .then(data => {
+            setEvents(prevEvents => [...prevEvents, {
+              start: moment.unix(data.start).toDate(),
+              end: moment.unix(data.end).toDate(),
+              title: data.title
+            }]);
+            fetchEvents();
+          });
+    }
+  }
+
   return (
-    <Calendar localizer={momentLocalizer(moment)} events={events} defaultDate={new Date()} defaultView="month" style={{ height: "100vh" }} />
+      <div>
+        <DnDCalendar
+            localizer={localizer}
+            events={events}
+            defaultDate={new Date()}
+            defaultView="week"
+            style={{ height: "100vh" }}
+            selectable={'ignoreEvents'}
+            onEventDrop={onEventDrop}
+            onEventResize={onEventResize}
+            resizable
+            onSelectEvent={handleSelectEvent}
+            onSelectSlot={handleSelectSlot}
+        />
+      <button className="addButton" onClick={toggleModal}>Add Event +</button>
+      {selectedEvent && (
+        <button className="deleteButton" onClick={handleDeleteEvent}>Delete Event</button>
+      )}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={toggleModal}>&times;</span>
+            <h2>Add Event</h2>
+            <div className="input-group">
+              <label htmlFor="eventTitle">Title</label>
+              <input id="eventTitle" value={title} onChange={e => setTitle(e.target.value)} placeholder="Title"/>
+            </div>
+            <div className="input-group">
+              <label htmlFor="eventDescription">Description</label>
+              <input id="eventDescription" value={description} onChange={e => setDescription(e.target.value)} placeholder="Description"/>
+            </div>
+            <div className="input-group">
+              <label htmlFor="startTime">Start Time</label>
+              <input id="startTime" type="datetime-local" value={startTime} onChange={e => setStartTime(e.target.value)}/>
+            </div>
+            <div className="input-group">
+              <label htmlFor="endTime">End Time</label>
+              <input id="endTime" type="datetime-local" value={endTime} onChange={e => setEndTime(e.target.value)}/>
+            </div>
+            <button onClick={handleAddEvent}>Save Event</button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
